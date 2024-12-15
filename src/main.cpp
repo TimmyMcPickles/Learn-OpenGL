46aa75975a726e9ee05f33df9920d1b16ba3bcd4
Ethan <kingscout1011@gmail.com>
2024-12-15 16:26:00 -0600

    Added comments to code, so myself and others can understand what this code does, and why it does it.

--- src/main.cpp
+++ src/main.cpp
@@ -2,18 +2,21 @@
 #include <GLFW/glfw3.h>
 #include <iostream>
 
+// settings
 const unsigned int SCR_WIDTH = 800;
 const unsigned int SCR_HEIGHT = 600;
 
 void framebuffer_size_callback(GLFWwindow* window, int width, int height);
 
 int main(){
+    // glfw init and config
     glfwInit();
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
     //glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
 
+    // window creation
     GLFWwindow* window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "LearnOpenGL", NULL, NULL);
 
     if (window == NULL){
@@ -22,6 +25,8 @@ int main(){
         return -1;
     }
     glfwMakeContextCurrent(window);
+
+    // Calls window resize function when window size changes
     glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
 
     if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)){
@@ -29,13 +34,16 @@ int main(){
         return -1;
     }
 
+    // rendering loop
     while (!glfwWindowShouldClose(window)){
-        glfwSwapBuffers(window);
+        glfwSwapBuffers(window); // applies double buffer to window  to prevent artifacts.
         glfwPollEvents();
     }
     glfwTerminate();
     return 0;
 }
+
+// Handles window resizing
 void framebuffer_size_callback(GLFWwindow* window, int width, int height){
     glViewport(0, 0, width, height);
 }
