1356bcb152421cb01a20170e14d63562817a3ca4
Ethan <kingscout1011@gmail.com>
2024-12-15 22:28:16 -0600

    red

--- src/main.cpp
+++ src/main.cpp
@@ -7,6 +7,7 @@ const unsigned int SCR_WIDTH = 800;
 const unsigned int SCR_HEIGHT = 600;
 
 void framebuffer_size_callback(GLFWwindow* window, int width, int height);
+void processInput(GLFWwindow *window);
 
 int main(){
     // glfw init and config
@@ -36,8 +37,13 @@ int main(){
 
     // rendering loop
     while (!glfwWindowShouldClose(window)){
+        //input
+        processInput(window);
+
         glfwSwapBuffers(window); // applies double buffer to window  to prevent artifacts.
         glfwPollEvents();
+        glClearColor(0.3f, 0.2f, 0.3f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
     }
     glfwTerminate();
     return 0;
@@ -46,4 +52,11 @@ int main(){
 // Handles window resizing
 void framebuffer_size_callback(GLFWwindow* window, int width, int height){
     glViewport(0, 0, width, height);
+}
+
+// Function for program input (like esc to close)
+void processInput(GLFWwindow *window){
+    if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS){
+        glfwSetWindowShouldClose(window, true);
+    }
 }
